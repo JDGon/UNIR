c43e74c3cae1388fd9bebe5dd9a98f674ec67411
Juan Diego <juandiego@pinflas.com>
2019-05-17 19:35:22 +0200

    Cambio a 0.11

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,11 +30,10 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        //double r = Math.random();
-        double r = 0.11;
+        double r = Math.random();
         if (r < 0.1) {
             fail("oops");
-        } else if (r < 0.2) {
+        } else if (r < 0.11) {
             throw new AssumptionViolatedException("skipping");
         }
     }
